64ad54c6ac744812ae73ca5215b1dffa0ba601ad
fuzzymfx <anubhabr50@gmail.com>
2023-05-16 22:52:42 +0530

    Added magnet-uri parser

--- src/downloadMag.ts
+++ src/downloadMag.ts
@@ -1,3 +1,12 @@
-export default async function downloadMagnet(magnetUrl: string) {
-	console.log("Downloading file from magnet: " + magnetUrl);
+import {
+	createReadStream,
+	createWriteStream,
+} from "fs";
+import MagnetUri from "magnet-uri";
+import magnet from "magnet-uri";;
+
+export default function downloadMagnet(magnetUrl: string) {
+	console.log("Downloading magnet from path: " + magnetUrl);
+	const parsed: MagnetUri.Instance = magnet(magnetUrl);
+	console.log(parsed);
 }
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import downloadFile from './downloadFile.js';
 import downloadMagnet from './downloadMag.js';
 
 const arg: string = process.argv[2];
-if(arg == "mag") downloadMagnet(process.argv[3]);
-else if(arg == "file") downloadFile(process.argv[3]);
-else{
-		console.log("Invalid argument");
-		process.exit(1);
+if (arg == "mag") {
+	console.log("Enter the magnet link: ");
+	process.stdin.on('data', function (data) {
+		const magnetUrl: string = data.toString().trim();
+		downloadMagnet(magnetUrl);
+	});
 }
+else if (arg == "file") {
+	console.log("Enter the file path: ");
+	process.stdin.on('data', function (data) {
+		const path: string = data.toString().trim();
+		downloadFile(path);
+	});
 
+}
+else {
+	console.log("Invalid argument: " + arg);
+}
